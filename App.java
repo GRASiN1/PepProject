b8a43211a9b293843102e33baf8908b321a8a8f0
GRASiN1 <gauravrajsingh047@gmail.com>
2024-07-11 15:17:42 +0530

    changed speed

--- App.java
+++ App.java
@@ -3,12 +3,14 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Scanner;
+
 import javax.imageio.ImageIO;
 import javax.swing.border.LineBorder;
 
 public class App {
-
-    final static int M = 4;
+    static Scanner sc = new Scanner(System.in);
+    final static int M = sc.nextInt();
     static JLabel[][] jLabel = new JLabel[M][M];
     static int board[][] = new int[M][M];
     static ImageIcon queenIcon;
@@ -24,7 +26,7 @@ public class App {
 
     static boolean isSafe(int row, int col) {
         try {
-            Thread.sleep(20); // Adjust sleep time for visualization speed
+            Thread.sleep(5);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -54,15 +56,15 @@ public class App {
 
         for (int i = 0; i < M; ++i) {
             try {
-                Thread.sleep(500); // Adjust sleep time for visualization speed
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
             if (isSafe(i, col)) {
                 board[i][col] = 1;
-                jLabel[i][col].setIcon(queenIcon); // Set queen icon instead of background color
-                jLabel[i][col].setBackground(Color.ORANGE); // Highlight the current placement
+                jLabel[i][col].setIcon(queenIcon);
+                jLabel[i][col].setBackground(Color.ORANGE);
 
                 foundSolution = findSolution(col + 1);
 
@@ -70,10 +72,8 @@ public class App {
                     return true;
                 } else {
                     board[i][col] = 0;
-                    jLabel[i][col].setIcon(null); // Clear icon for backtracking
-                    jLabel[i][col].setBackground((i + col) % 2 == 0 ? Color.WHITE : Color.LIGHT_GRAY); // Restore
-                                                                                                       // chessboard
-                                                                                                       // color
+                    jLabel[i][col].setIcon(null);
+                    jLabel[i][col].setBackground((i + col) % 2 == 0 ? Color.WHITE : Color.LIGHT_GRAY);
                 }
             }
         }
@@ -83,28 +83,26 @@ public class App {
 
     static void solveNQueen() {
         try {
-            Thread.sleep(500); // Initial delay for setup
+            Thread.sleep(1);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        // Reset board and icons
         for (int i = 0; i < M; ++i) {
             for (int j = 0; j < M; ++j) {
                 try {
-                    Thread.sleep(20); // Adjust sleep time for visualization speed
+                    Thread.sleep(5);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 board[i][j] = 0;
-                jLabel[i][j].setIcon(null); // Clear any existing icons
-                jLabel[i][j].setBackground((i + j) % 2 == 0 ? Color.WHITE : Color.LIGHT_GRAY); // Alternate colors
+                jLabel[i][j].setIcon(null);
+                jLabel[i][j].setBackground((i + j) % 2 == 0 ? Color.WHITE : Color.LIGHT_GRAY);
             }
         }
 
-        // If solution exists, find and display it
         if (findSolution(0)) {
-            // Show dialog box for solution found
+
             JOptionPane.showMessageDialog(null, "Solution found!", "N-Queens Solution",
                     JOptionPane.INFORMATION_MESSAGE);
         } else {
@@ -121,7 +119,6 @@ public class App {
         chessboardPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         jFrame.add(chessboardPanel);
 
-        // Load queen image from URL
         try {
             URL imageUrl = new URL(
                     "https://imgs.search.brave.com/Z9wN86G-Ff35cfPjz_sF7E6eM3a5uHB4jhtV3WmvO30/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5nYWxsLmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvMS9RdWVl/bi1QTkctRmlsZS5w/bmc");
@@ -135,15 +132,13 @@ public class App {
             e.printStackTrace();
         }
 
-        // Create chessboard with custom grid lines
         for (int i = 0; i < M; ++i) {
             for (int j = 0; j < M; ++j) {
                 jLabel[i][j] = new JLabel();
                 jLabel[i][j].setHorizontalAlignment(SwingConstants.CENTER);
                 jLabel[i][j].setOpaque(true);
-                jLabel[i][j].setBackground((i + j) % 2 == 0 ? Color.WHITE : Color.LIGHT_GRAY); // Alternate colors
+                jLabel[i][j].setBackground((i + j) % 2 == 0 ? Color.WHITE : Color.LIGHT_GRAY);
 
-                // Add borders to simulate grid lines
                 jLabel[i][j].setBorder(new LineBorder(Color.BLACK));
 
                 chessboardPanel.add(jLabel[i][j]);
